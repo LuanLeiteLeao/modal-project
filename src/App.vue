0b3b94801205ee38a20f545481df5dfa35714d9b
luanleiteleao <luanleiteraio@gmail.com>
2023-12-01 17:28:12 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <h1>{{ title }}</h1>
+  <Modal/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'My first Vue App :)'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
